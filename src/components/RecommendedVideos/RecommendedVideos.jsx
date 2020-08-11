25896698b0647f72ff804366740ca4bff29a76d3
abdennour001 <amokranabdennour@gmail.com>
2020-08-11 10:05:56 +0100

    finishing up recommended ✅

--- src/components/RecommendedVideos/RecommendedVideos.jsx
+++ src/components/RecommendedVideos/RecommendedVideos.jsx
@@ -17,12 +17,39 @@ function RecommendedVideos() {
                     duration="8:38"
                 />
                 <VideoCard
-                    thumbnail="https://i.ytimg.com/vi/ciGTML-jukM/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDCpDRbv0JS06H5UO4hnFGYnm2viw"
-                    channel="Shantanu Kumar"
-                    channelImage="https://yt3.ggpht.com/a/AATXAJyb_Q6mAuRxbMoRwN8QojxHKmKyGMibjk7rF1gI2ag=s176-c-k-c0xffffffff-no-rj-mo"
-                    title="Designing an Asian Soda Brand"
-                    views="82,379 views"
-                    timestamp="21 Jun 2020"
+                    thumbnail="https://i.ytimg.com/vi/76d2yOMXmag/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDazOL3vguH3dxH91yREzTHlASi-w"
+                    channel="Coding Garden with CJ"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJyd7g6PJDaD3j2Ouariuf_U42P-yZZPQw6-E-B08Q=s176-c-k-c0xffffffff-no-rj-mo"
+                    title="[ARCHIVE] 🕸⛏ Scrape the Web with Node.js, Finding Undocumented APIs and Reversing Engineering D"
+                    views="120 watching now"
+                    timestamp="2 hours ago"
+                    duration="LIVE"
+                />
+                <VideoCard
+                    thumbnail="https://i.ytimg.com/vi/FVtFcbBfNYw/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBOfwg6P9l3aGnhxYg4qGF-i9PHew"
+                    channel="EL HAMA"
+                    channelImage="https://yt3.ggpht.com/a-/AOh14Gj-OdlYSeHv6JcxFZVJLpfGYHzJQ529ZYHdgg=s68-c-k-c0x00ffffff-no-rj-mo"
+                    title="Kaleida - Think (Lyrics) 'John Wick soundtrack'"
+                    views="1,7M views"
+                    timestamp="23 Aug 2018"
+                    duration="3:56"
+                />
+                <VideoCard
+                    thumbnail="https://i.ytimg.com/vi/vuT_bXzhqhY/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLC-IP0C9QuF34appU16VXnuoqOevw"
+                    channel="The Weekend 🎵"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJwtg4RzEN6Tnk6o3Pl6EEW9zmKJJ6687BWynExA=s176-c-k-c0xffffffff-no-nd-rj"
+                    title="Starboy (Live From The Victoria’s Secret Fashion Show 2016 in Paris)"
+                    views="123M views"
+                    timestamp="6 Dec 2016"
+                    duration="4:03"
+                />
+                <VideoCard
+                    thumbnail="https://i.ytimg.com/vi/LN668OAUrK4/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLC_Nun_X0a7_lush6q_BYY6Z5rgEg"
+                    channel="The Flutter Way"
+                    channelImage="https://yt3.ggpht.com/a-/AOh14GgP4eZ-XjGzOhV65oMKh1wfFqIxcFaDCmTlNA=s68-c-k-c0x00ffffff-no-rj-mo"
+                    title="Online Shop App - Flutter UI - Speed Code"
+                    views="67K views"
+                    timestamp="3 Jul 2020"
                     duration="4:50"
                 />
                 <VideoCard
@@ -32,6 +59,42 @@ function RecommendedVideos() {
                     title="Designing an Asian Soda Brand"
                     views="82,379 views"
                     timestamp="21 Jun 2020"
+                    duration="8:38"
+                />
+                <VideoCard
+                    thumbnail="https://i.ytimg.com/vi/76d2yOMXmag/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDazOL3vguH3dxH91yREzTHlASi-w"
+                    channel="Coding Garden with CJ"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJyd7g6PJDaD3j2Ouariuf_U42P-yZZPQw6-E-B08Q=s176-c-k-c0xffffffff-no-rj-mo"
+                    title="[ARCHIVE] 🕸⛏ Scrape the Web with Node.js, Finding Undocumented APIs and Reversing Engineering D"
+                    views="120 watching now"
+                    timestamp="2 hours ago"
+                    duration="LIVE"
+                />
+                <VideoCard
+                    thumbnail="https://i.ytimg.com/vi/FVtFcbBfNYw/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBOfwg6P9l3aGnhxYg4qGF-i9PHew"
+                    channel="EL HAMA"
+                    channelImage="https://yt3.ggpht.com/a-/AOh14Gj-OdlYSeHv6JcxFZVJLpfGYHzJQ529ZYHdgg=s68-c-k-c0x00ffffff-no-rj-mo"
+                    title="Kaleida - Think (Lyrics) 'John Wick soundtrack'"
+                    views="1,7M views"
+                    timestamp="23 Aug 2018"
+                    duration="3:56"
+                />
+                <VideoCard
+                    thumbnail="https://i.ytimg.com/vi/vuT_bXzhqhY/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLC-IP0C9QuF34appU16VXnuoqOevw"
+                    channel="The Weekend 🎵"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJwtg4RzEN6Tnk6o3Pl6EEW9zmKJJ6687BWynExA=s176-c-k-c0xffffffff-no-nd-rj"
+                    title="Starboy (Live From The Victoria’s Secret Fashion Show 2016 in Paris)"
+                    views="123M views"
+                    timestamp="6 Dec 2016"
+                    duration="4:03"
+                />
+                <VideoCard
+                    thumbnail="https://i.ytimg.com/vi/LN668OAUrK4/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLC_Nun_X0a7_lush6q_BYY6Z5rgEg"
+                    channel="The Flutter Way"
+                    channelImage="https://yt3.ggpht.com/a-/AOh14GgP4eZ-XjGzOhV65oMKh1wfFqIxcFaDCmTlNA=s68-c-k-c0x00ffffff-no-rj-mo"
+                    title="Online Shop App - Flutter UI - Speed Code"
+                    views="67K views"
+                    timestamp="3 Jul 2020"
                     duration="4:50"
                 />
                 <VideoCard
@@ -41,33 +104,42 @@ function RecommendedVideos() {
                     title="Designing an Asian Soda Brand"
                     views="82,379 views"
                     timestamp="21 Jun 2020"
-                    duration="4:50"
+                    duration="8:38"
                 />
                 <VideoCard
-                    thumbnail="https://i.ytimg.com/vi/ciGTML-jukM/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDCpDRbv0JS06H5UO4hnFGYnm2viw"
-                    channel="Shantanu Kumar"
-                    channelImage="https://yt3.ggpht.com/a/AATXAJyb_Q6mAuRxbMoRwN8QojxHKmKyGMibjk7rF1gI2ag=s176-c-k-c0xffffffff-no-rj-mo"
-                    title="Designing an Asian Soda Brand"
-                    views="82,379 views"
-                    timestamp="21 Jun 2020"
-                    duration="4:50"
+                    thumbnail="https://i.ytimg.com/vi/76d2yOMXmag/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDazOL3vguH3dxH91yREzTHlASi-w"
+                    channel="Coding Garden with CJ"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJyd7g6PJDaD3j2Ouariuf_U42P-yZZPQw6-E-B08Q=s176-c-k-c0xffffffff-no-rj-mo"
+                    title="[ARCHIVE] 🕸⛏ Scrape the Web with Node.js, Finding Undocumented APIs and Reversing Engineering D"
+                    views="120 watching now"
+                    timestamp="2 hours ago"
+                    duration="LIVE"
                 />
                 <VideoCard
-                    thumbnail="https://i.ytimg.com/vi/ciGTML-jukM/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDCpDRbv0JS06H5UO4hnFGYnm2viw"
-                    channel="Shantanu Kumar"
-                    channelImage="https://yt3.ggpht.com/a/AATXAJyb_Q6mAuRxbMoRwN8QojxHKmKyGMibjk7rF1gI2ag=s176-c-k-c0xffffffff-no-rj-mo"
-                    title="Designing an Asian Soda Brand"
-                    views="82,379 views"
-                    timestamp="21 Jun 2020"
-                    duration="4:50"
+                    thumbnail="https://i.ytimg.com/vi/FVtFcbBfNYw/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBOfwg6P9l3aGnhxYg4qGF-i9PHew"
+                    channel="EL HAMA"
+                    channelImage="https://yt3.ggpht.com/a-/AOh14Gj-OdlYSeHv6JcxFZVJLpfGYHzJQ529ZYHdgg=s68-c-k-c0x00ffffff-no-rj-mo"
+                    title="Kaleida - Think (Lyrics) 'John Wick soundtrack'"
+                    views="1,7M views"
+                    timestamp="23 Aug 2018"
+                    duration="3:56"
                 />
                 <VideoCard
-                    thumbnail="https://i.ytimg.com/vi/ciGTML-jukM/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDCpDRbv0JS06H5UO4hnFGYnm2viw"
-                    channel="Shantanu Kumar"
-                    channelImage="https://yt3.ggpht.com/a/AATXAJyb_Q6mAuRxbMoRwN8QojxHKmKyGMibjk7rF1gI2ag=s176-c-k-c0xffffffff-no-rj-mo"
-                    title="Designing an Asian Soda Brand"
-                    views="82,379 views"
-                    timestamp="21 Jun 2020"
+                    thumbnail="https://i.ytimg.com/vi/vuT_bXzhqhY/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLC-IP0C9QuF34appU16VXnuoqOevw"
+                    channel="The Weekend 🎵"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJwtg4RzEN6Tnk6o3Pl6EEW9zmKJJ6687BWynExA=s176-c-k-c0xffffffff-no-nd-rj"
+                    title="Starboy (Live From The Victoria’s Secret Fashion Show 2016 in Paris)"
+                    views="123M views"
+                    timestamp="6 Dec 2016"
+                    duration="4:03"
+                />
+                <VideoCard
+                    thumbnail="https://i.ytimg.com/vi/LN668OAUrK4/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLC_Nun_X0a7_lush6q_BYY6Z5rgEg"
+                    channel="The Flutter Way"
+                    channelImage="https://yt3.ggpht.com/a-/AOh14GgP4eZ-XjGzOhV65oMKh1wfFqIxcFaDCmTlNA=s68-c-k-c0x00ffffff-no-rj-mo"
+                    title="Online Shop App - Flutter UI - Speed Code"
+                    views="67K views"
+                    timestamp="3 Jul 2020"
                     duration="4:50"
                 />
             </div>
